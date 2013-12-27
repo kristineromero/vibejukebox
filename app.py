59307bbf7457f111aa277924e424c60467748f16
kristineromero <kristine.romero2@gmail.com>
2013-12-27 17:12:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ app = Flask(__name__)
 def getArtists():
     
     songs = request.json['songs']
+    return jsonify( { 'songs': songs} )
     
+if __name__ == '__main__':
+    app.run(debug = True)
+
+"""
     ###############################################################
     ################## SPOTIFY ID  FUNCTION #######################
 
@@ -35,7 +40,7 @@ def getArtists():
     return jsonify( { 'artistid': find_artist_final} )
     
     
-"""    
+    
     ###########################################################
     ############### WEIGHTING FUNCTION #######################
     
